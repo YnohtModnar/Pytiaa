83ae12b45bd05ce5508dcd6eacfe6173d5635b7d
waffles <debroise.quentin@gmail.com>
2016-03-27 15:59:34 +0200

    Animation Paire based algorithm

--- Algorithm/PaireBased.py
+++ Algorithm/PaireBased.py
@@ -3,16 +3,17 @@ import math
 import pylab
 import matplotlib.pyplot as plt
 
+from random import choice
 from Pytiaa.utils import dist
 
-def SimpleAnalogical(new: tuple, points: tuple, k: int=1):
-	c, points, classe = _nearest_neighbors(new, points)
+def PaireBased(new: tuple, points: tuple, k: int=1):
+	c, points, classe = _nearest_neighbor(new, points)
 	if(classe is None):
 		couples = _couples_creation(c, points)
 		classe = _compute_class(couples, c)
 	return classe, c, couples
 
-def _nearest_neighbors(new, points):
+def _nearest_neighbor(new, points):
 	classe = None
 	# Compute the nearest neighbor
 	c = (points[0], dist(new[0], points[0][0], new[1], points[0][1]))
@@ -22,7 +23,7 @@ def _nearest_neighbors(new, points):
 			c = (p, distance)
 	points.remove(c[0])
 
-	# verifier si il est égal on donne direct la classe
+	# If the NN and the new point are the same, their class is the same
 	if(new[0] == c[0][0] and new[1] == c[0][1]):
 		classe = c[0][2]
 
@@ -73,25 +74,60 @@ def _reset(ax, points, c):
 	ax.scatter(c[0][0], c[0][1], c=c[0][2])
 
 def draw(new, points, c, couples, classe, plt):
+	NB_COUPLES_DISPLAYED = 4
 	fig, ax = plt.subplots()
 
+	# IMAGE 1 #
 	_reset(ax, points, c)
 	pylab.savefig('img1')
 
+	# IMAGE 2 #
 	ax.scatter(new[0], new[1], c="#000000")
 	pylab.savefig('img2')
 
+	# IMAGE 3 #
 	ax.plot([new[0], c[0][0]], [new[1], c[0][1]], c="#878787", alpha=.3)
 	pylab.text(0.5, 1.05, 'Nearest neighbor, dist='+str(round(c[1], 4)), fontsize=12)
 	pylab.savefig('img3')
 
+	# IMAGE 4 #
+	# Clear and redraw the points, axes, ...
 	ax.clear()
 	_reset(ax, points, c)
 	ax.scatter(new[0], new[1], c="#000000")
-	pylab.text(0.5, 1.05, 'Couples creation', fontsize=12)
-	for i in range(len(couples)):
-		ax.plot([couples[i][0][0], couples[i][1][0]], [couples[i][0][1], couples[i][1][1]], c="#878787", alpha=.3)
+	pylab.text(0.5, 1.05, 'Couples creation + distance computation', fontsize=12)
+
+	# Select random couples to display
+	displayedCouples = [choice(couples) for i in range(NB_COUPLES_DISPLAYED)]
+	for c in [choice(couples) for i in range(NB_COUPLES_DISPLAYED)]:
+		ax.plot([c[0][0], c[1][0]], [c[0][1], c[1][1]], c="#878787", alpha=.3)	# Draw the line between the two points
+		midx, midy = (c[0][0] + c[1][0]) / 2, (c[0][1] + c[1][1]) / 2	# Where the text is placed
+		pylab.text(midx, midy, str(round(c[2], 3)))	 # Display the distance between the two points
 	pylab.savefig('img4')
+
+	# IMAGE 5 #
+	if(classe is None):
+		print("Can't classify")
+	print("Solving analogical equation, first solved => class given to the new point")
+	# Clear and redraw the points, axes, ...
+	ax.clear()
+	_reset(ax, points, c)
+	ax.scatter(new[0], new[1], c="#000000")
+	# pylab.text(0.5, 1.05, 'Couples creation + distance computation', fontsize=12)
+
+	for i, closest in enumerate(couples):
+		# Circles to highlight the current points of the equation
+		w, x, y = closest[0], closest[1], c[0]
+		p1 = ax.add_patch(plt.Circle((w[0], w[1]), radius=0.02, color='#FF0000'))
+		p2 = ax.add_patch(plt.Circle((x[0], x[1]), radius=0.02, color='#FF0000'))
+		p3 = ax.add_patch(plt.Circle((y[0], y[1]), radius=0.02, color='#FF0000'))
+		textEquation = pylab.text(.5, 1.05, str(w[-1]) + " : " + str(x[-1]) + " :: " + str(y[-1]) + " : x")
+		# print(str(w[-1]) + " : " + str(x[-1]) + " :: " + str(y[-1]) + " : x")
+		pylab.savefig('img' + str(5 + i))
+		# Clear the circles & txt
+		for patch in [p1, p2, p3]:
+			patch.remove()
+		textEquation.remove()
 
 def main(argv):
 	points = [
@@ -104,7 +140,7 @@ def main(argv):
 	]
 	new = (.5, .5)
 
-	classe, c, couples = SimpleAnalogical(new ,points)
+	classe, c, couples = PaireBased(new ,points)
 	print(classe)
 	draw(new, points, c, couples, classe, plt)
 
