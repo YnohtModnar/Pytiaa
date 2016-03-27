a36695f6db2be4a190afc5cdbd7d3f31a6449dad
waffles <debroise.quentin@gmail.com>
2016-03-27 18:28:10 +0200

    -

--- Algorithm/Fadana.py
+++ Algorithm/Fadana.py
@@ -93,8 +93,10 @@ def classCalcul(points : list, triplets : list, analogicalDiff : list):
     return classes
 
 def _draw(new: tuple, points: tuple, triplets: tuple, anaDiff: tuple, cl: str, plt):
+    NB_TRIPLET_DISPLAYED = 4
     fig, ax = plt.subplots()
 
+    # IMAGE 1 #
     # Training set
     ax.scatter(
         [point[0] for point in points],
@@ -103,50 +105,75 @@ def _draw(new: tuple, points: tuple, triplets: tuple, anaDiff: tuple, cl: str, p
     )
     pylab.savefig('img1')
 
+    # IMAGE 2 #
     # Triplets
-    tplt = []
-    for i in range(3):
-        r = randrange(0, len(triplets))
-        tplt.append(triplets[r])
-        triplets.remove(triplets[r])
-    for i, t in enumerate(tplt):
-        ax.clear()
-        ax.scatter(
-            [point[0] for point in points],
-            [point[1] for point in points],
-            c=[point[2] for point in points]
-        )
-        ax.add_patch(plt.Circle((points[t[1]][0], points[t[1]][1]), radius=0.02, color='#FF0000'))
-        ax.add_patch(plt.Circle((points[t[2]][0], points[t[2]][1]), radius=0.02, color='#FF0000'))
-        ax.add_patch(plt.Circle((points[t[3]][0], points[t[3]][1]), radius=0.02, color='#FF0000'))
+    tripletsDisplayed = [choice(triplets) for i in range(NB_TRIPLET_DISPLAYED)]
+    # Highlights triplets
+    label = ['A', 'B', 'C']
+    for i, t in enumerate(tripletsDisplayed):
+        patchs = [ax.add_patch(plt.Circle((points[t[i+1]][0], points[t[i+1]][1]), radius=0.02, color='#FF0000')) for i in range(3)]
+        texts = [pylab.text(points[t[i+1]][0], points[t[i+1]][1], label[i]) for i in range(3)]
         pylab.savefig('img'+str(i+2))
+        # Clear circles and texts
+        for i in range(3):
+            patchs[i].remove()
+            texts[i].remove()
 
-    # Meilleurs triplets
+    # IMAGE 3 #
+    # computes analogical difference
     # print(anaDiff)
     print("On trouve les meilleurs triplets en résolvant le calcul de différence analogique")
+    print(tripletsDisplayed[0])
+    txt = pylab.text(.5, 1.05, 'Find best triplets solving analogical difference')
+    a = points[tripletsDisplayed[0][1]]
+    b = points[tripletsDisplayed[0][2]]
+    c = points[tripletsDisplayed[0][3]]
+    patchs = [ax.add_patch(plt.Circle((points[tripletsDisplayed[0][i+1]][0],
+                                       points[tripletsDisplayed[0][i+1]][1]),
+                                       radius=0.02,
+                                       color='#FF0000')) for i in range(3)]
+    pylab.savefig('img5')
+    txt.remove()
 
-    print(triplets[anaDiff[0][0]])
-    t = triplets[anaDiff[0][0]]
-    print(t[0])
-    print(t)
-    print(anaDiff[0][1])
-    print("On calcul la difference analogique de chaque composante (valeurs normalisées)")
-    adx1 = points[t[1]][0] - points[t[2]][0]                # A - B
-    ady1 = points[t[1]][1] - points[t[2]][1]
-    print("Ax - Bx = " + str(adx1))
-    print("Ay - By = " + str(ady1))
+    # IMAGE 4 #
+    # print("On calcul la difference analogique de chaque composante (valeurs normalisées)")
+    txt = pylab.text(.5, 1.05, 'A - B, on each attribut')
+    adx1 = a[0] - b[0]                # A - B
+    ady1 = a[1] - b[1]
+    txt1 = pylab.text(.1, 1.05, "Ax - Bx = " + str(adx1))
+    txt2 = pylab.text(.1, 1.05, "Ay - By = " + str(ady1))
+    pylab.savefig('img6')
+    # print("Ax - Bx = " + str(adx1))
+    # print("Ay - By = " + str(ady1))
     # Analogical difference C and D
-    adx2 = points[t[3]][0] - new[0]                         # C - D
-    ady2 = points[t[3]][1] - new[1]
-    print("Cx - Dx = " + str(adx2))
-    print("Cy - Dy = " + str(ady2))
+    txt.remove()
+    txt1.remove()
+    txt2.remove()
+
+    txt = pylab.text(.5, 1.05, 'C - D, on each attribut')
+    adx2 = c[0] - new[0]                         # C - D
+    ady2 = c[1] - new[1]
+    txt1 = pylab.text(.1, 1.05, "Cx - Dx = " + str(adx2))
+    txt2 = pylab.text(.1, 1.05, "Cy - Dy = " + str(ady2))
+    # print("Cx - Dx = " + str(adx2))
+    # print("Cy - Dy = " + str(ady2))
+    pylab.savefig('img7')
+    txt.remove()
+    txt1.remove()
+    txt2.remove()
+
     # Real analogical difference
+    # pylab.text('AD = 1 - | (a - B) - (C - D) |, on each attribute, then sum everything to get the final analogical difference')
     adx = 1 - abs(adx1 - adx2)                              # AD = 1 - | (A - B) - (C - D) |
     print("AD(Ax, Bx) = 1 - | (Ax - Bx) - (Cx - Dx) | = " + str(adx))
     ady = 1 - abs(ady1 - ady2)
     print("AD(Ay, By) = 1 - | (Ay - By) - (Cy - Dy) | = " + str(ady))
     ad = adx + ady
     print("AD(A, B, C, D) Finale = " + str(ad))
+
+    txt = pylab.text(.5, 1.05, "AD(A, B, C, D) Finale = " + str(ad))
+    pylab.savefig('img8')
+    txt.remove()
 
 
 def main(argv):
@@ -162,7 +189,7 @@ def main(argv):
     print("Class :", classe)
 
 
-    # plt.show()
+        # plt.show()
 
 if(__name__ == "__main__"):
     sys.exit(main(sys.argv))
